8b149c2c863b6dd2136e2b210356677dd0464258
josegill12 <jose.gill1988@gmail.com>
2023-11-19 15:08:14 -0700

    maybe

--- backend/controllers/tripsController.js
+++ backend/controllers/tripsController.js
@@ -1,19 +1,57 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
+const Trip = require('../models/trips');
 
-
-
-//signup
-router.get('/signup', (req, res) => {
-    res.render('auth/signup')
+// index
+router.get('/', async (req, res) => {
+    try {
+        res.json(await Trip.find());
+    } catch (error) {
+        res.status(400).json(error);
+    }
 });
 
-//logout
-router.get('/logout', (req, res) => {
-    req.session.destroy();
-    res.redirect('/')
+// create
+router.post('/', async (req, res) => {
+    try {
+        res.json(await Trip.create(req.body));
+    } catch (error) {
+        res.status(400).json(error);
+    }
 });
 
+// delete
+router.delete("/:id", async (req, res) => {
+    try {
+        res.json(
+            await Trip.findByIdAndDelete(req.params.id, req.body, { new: true })
+        );
+    } catch (error) {
+        res.status(400).json(error);
+    }
+});
+
+
+// update
+
+router.put("/:id", async (req, res) => {
+  try {
+    res.json(
+      await Trip.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    );
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
+
+// show
+router.get("/:id", async (req, res) => {
+  try {
+    res.json(await Trip.findById(req.params.id));
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
 
 module.exports = router;
